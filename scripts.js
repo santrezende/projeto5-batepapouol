59c553b266f7c50a0d4dd262b486de4974a7cdab
santiago <santiagorsilva99@gmail.com>
2023-01-19 22:18:37 -0300

    versao com data-test e um pouco de JS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,110 @@
+let user = {
+    name: prompt('Qual o seu nome?')
+};
+
+function fazerLogIn() {
+    const requestUser = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', user);
+    let response;
+
+    function processarResposta(resposta) {
+
+    }
+
+    function deuRuim(resposta) {
+        response = resposta;
+        alert('ja tem um user com esse nome, escolhe outro');
+        user = {
+            name: prompt('Qual o seu nome?')
+        };
+        fazerLogIn();
+    }
+
+    requestUser.then(processarResposta);
+    requestUser.catch(deuRuim);
+}
+
+let container = document.querySelector(".container-mensagens");
+
+function carregaMensagens() {
+    container.innerHTML = "";
+    let mensagens;
+    function processarResposta(resposta) {
+        mensagens = resposta.data;
+        for (let index = 0; index < mensagens.length; index++) {
+
+            function criaMensagemTodos() {
+                const div = `<div data-test="message" class="print todos">
+                <span class="texto-horario">(${mensagens[index].time})</span> <span class="texto-usuario">${mensagens[index].from}</span> para <span
+                    class="texto-destinatario">${mensagens[index].to}: </span><span>${mensagens[index].text}</span>
+            </div>`;
+
+                container.innerHTML = container.innerHTML + div;
+            }
+            function criaMensagemTodosLast() {
+                const div = `<div data-test="message" class="print todos last">
+                <span class="texto-horario">(${mensagens[index].time})</span> <span class="texto-usuario">${mensagens[index].from}</span> para <span
+                    class="texto-destinatario">${mensagens[index].to}: </span><span>${mensagens[index].text}</span>
+            </div>`;
+
+                container.innerHTML = container.innerHTML + div;
+            }
+            function criaAviso() {
+                const div = `<div data-test="message" class="print aviso">
+                <span class="texto-horario">(${mensagens[index].time})</span> <span class="texto-usuario">${mensagens[index].from} </span><span>${mensagens[index].text}</span>
+            </div>`;
+
+                container.innerHTML = container.innerHTML + div;
+            }
+            function criaAvisoLast() {
+                const div = `<div data-test="message" class="print aviso last">
+                <span class="texto-horario">(${mensagens[index].time})</span> <span class="texto-usuario">${mensagens[index].from} </span><span>${mensagens[index].text}</span>
+            </div>`;
+
+                container.innerHTML = container.innerHTML + div;
+            }
+            function criaMensagemPrivada() {
+                const div = `<div data-test="message" class="print privado">
+                <span class="texto-horario">(${mensagens[index].time})</span> <span class="texto-usuario">${mensagens[index].from}</span> reservadamente para
+                <span class="texto-destinatario">${mensagens[index].to}</span><span>: ${mensagens[index].text}</span>
+            </div>`;
+
+                container.innerHTML = container.innerHTML + div;
+            }
+            function criaMensagemPrivadaLast() {
+                const div = `<div data-test="message" class="print privado last">
+                <span class="texto-horario">(${mensagens[index].time})</span> <span class="texto-usuario">${mensagens[index].from}</span> reservadamente para
+                <span class="texto-destinatario">${mensagens[index].to}</span><span>: ${mensagens[index].text}</span>
+            </div>`;
+
+                container.innerHTML = container.innerHTML + div;
+            }
+
+            if (mensagens[index].type === "status" && index === 99) {
+                criaAvisoLast();
+            } else if (mensagens[index].type === "status" && index !== 99) {
+                criaAviso();
+            }
+            if (mensagens[index].type === "message" && index === 99) {
+                criaMensagemTodosLast();
+            } else if (mensagens[index].type === "message" && index !== 99) {
+                criaMensagemTodos();
+            }
+            if (mensagens[index].type === "private_message" && index === 99) {
+                criaMensagemPrivadaLast();
+            } else if (mensagens[index].type === "status" && index !== 99) {
+                criaMensagemPrivada();
+            }
+        }
+        const teste = document.querySelector(".last");
+        teste.scrollIntoView();
+    }
+
+    const requestMensagens = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    requestMensagens.then(processarResposta);
+}
+
+setInterval(carregaMensagens, 3000);
+
 function teste() {
     const painel = document.querySelector(".segunda-tela");
     painel.classList.remove("escondido");
@@ -8,41 +115,7 @@ function volta() {
     painel.classList.add("escondido");
 }
 
-function limpaTexto() {
-    const input = document.querySelector("textarea");
+function limpaTexto(caixaDeTexto) {
+    const input = caixaDeTexto;
     input.innerHTML = "";
-}
-
-const container = document.querySelector(".container-mensagens");
-
-function criarElementos(tag, classe) {
-    const elemento = document.createElement(tag);
-    elemento.className = classe;
-    return (elemento);
-}
-
-function criaMensagemTodos() {
-    const div = `<div class="print todos">
-    <span class="texto-horario">(09:21:45)</span> <span class="texto-usuario">João</span> para <span
-        class="texto-destinatario">Todos</span><span>: Bom dia</span>
-</div>`;
-
-    container.innerHTML = container.innerHTML + div;
-}
-
-function criaAviso() {
-    const div = `<div class="print aviso">
-    <span class="texto-horario">(09:21:45)</span> <span class="texto-usuario">João</span> entra na sala...
-</div>`;
-
-    container.innerHTML = container.innerHTML + div;
-}
-
-function criaMensagemPrivada() {
-    const div = `<div class="print privado">
-    <span class="texto-horario">(09:21:45)</span> <span class="texto-usuario">João</span> reservadamente para
-    <span class="texto-destinatario">Maria</span><span>: Oi gatinha quer tc? d</span>
-</div>`;
-
-    container.innerHTML = container.innerHTML + div;
 }
